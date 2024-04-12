78c5b8ecfa6ec300c08398be381684d64272495a
OriAvr <ori.avr123@gmail.com>
2024-04-12 18:26:56 +0300

    Adding variables

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function fetchCourses() {
-  fetch("http://localhost:3000/courses")
+  fetch(`http://${PRIVATE_IP}:3000/courses`)
     .then((response) => response.json())
     .then((data) => populateTable(data))
     .catch((error) => console.error("Error fetching courses:", error));
